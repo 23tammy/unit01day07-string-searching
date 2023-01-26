bdf00505edaabe86dda6ba941546b6e42caccb2b
tammy vu <tammy.vsk@gmail.com>
2023-01-26 12:02:27 -0600

    added no tag found conditional

--- FindTagContents.java
+++ FindTagContents.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class FindTagContents {
     public static void main(String[] args) {
-        
+        System.out.println("INSERT HTML:");
+        Scanner scanner = new Scanner(System.in);
+        String html = scanner.nextLine();
+
+        System.out.println("INSERT TAG:");
+        String tag = scanner.nextLine();
+
+        int htmli = html.indexOf(tag);
+        if (htmli == -1){
+            System.out.println("NO TAG FOUND.");
+        }else{
+            int aftertag = html.indexOf(">", htmli);
+            int beforetag = html.indexOf("</", aftertag);
+
+            System.out.print(html.substring(aftertag + 1, beforetag));
+        }
+
+        scanner.close();
+
     }
 }
