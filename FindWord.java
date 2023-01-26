dfd56ad10d024d6070adf368a25bf0c384ebe29d
tammy vu <tammy.vsk@gmail.com>
2023-01-26 12:36:15 -0600

    fixed index order

--- FindWord.java
+++ FindWord.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class FindWord {
     public static void main(String[] args) {
-        
+        System.out.println("INSERT SENTENCE:");
+        Scanner scanner = new Scanner(System.in);
+        String sentence = scanner.nextLine();
+
+        System.out.println("INSERT LETTER:");
+        String letter = scanner.nextLine();
+
+        int letteri = sentence.indexOf(letter);
+
+        if (letteri != -1){
+            int afterword = sentence.indexOf(" ", letteri);
+            String sentBeforeLet = sentence.substring(0, afterword);
+            int beforeword = sentBeforeLet.lastIndexOf(" ");
+
+            System.out.print(sentBeforeLet.substring(beforeword+1, afterword));
+        }else{
+            System.out.println("NO TAG FOUND.");
+            
+        }
+        scanner.close();
+
     }
 }
