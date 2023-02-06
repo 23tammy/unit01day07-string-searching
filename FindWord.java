39fde52e7f8967534c74a25c08f361fc4baae8ac
tammy vu <tammy.vsk@gmail.com>
2023-02-06 12:15:06 -0600

    fixed bug

--- FindWord.java
+++ FindWord.java
@@ -1,8 +1,3 @@
-// BUG: the 3rd example (searching for "d" caused this to happen:)
-/* Exception in thread "main" java.lang.StringIndexOutOfBoundsException: begin 0, end -1, length 26
-        at java.base/java.lang.String.checkBoundsBeginEnd(String.java:3319)
-        at java.base/java.lang.String.substring(String.java:1874)
-        at FindWord.main(FindWord.java:16) */
 import java.util.Scanner;
 
 public class FindWord {
@@ -16,7 +11,13 @@ public class FindWord {
 
         int letteri = sentence.indexOf(letter);
 
-        if (letteri != -1){
+        if (letteri == sentence.length()-1){
+            String sentBeforeLet = sentence.substring(0, sentence.length());
+            int beforeword = sentBeforeLet.lastIndexOf(" ");
+
+            System.out.print(sentBeforeLet.substring(beforeword+1, sentence.length()));
+        }
+        else if(letteri != -1){
             int afterword = sentence.indexOf(" ", letteri);
             String sentBeforeLet = sentence.substring(0, afterword);
             int beforeword = sentBeforeLet.lastIndexOf(" ");
@@ -27,6 +28,5 @@ public class FindWord {
             
         }
         scanner.close();
-
     }
 }
